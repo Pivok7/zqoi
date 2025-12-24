808b22612da37a70f4999b0270a6cc5bdfa408c1
Pivok <pivoc@protonmail.com>
2025-12-25 00:38:56 +0100

    Add args to benchmark

--- benchmark/zqoi/src/main.zig
+++ benchmark/zqoi/src/main.zig
@@ -10,21 +10,29 @@ pub const debug_mode = switch (builtin.mode) {
     else => false,
 };
 
-const EncDecTime = struct{
+const EncDecTime = struct {
     encode: i128,
     decode: i128,
 };
 
-const BenchmarkOptions = struct{
-    iters: usize = 10,
+const BenchmarkOptions = struct {
+    runs: usize = 10,
     print_individual: bool = false,
-    warmup: bool = true,
+    warmup: usize = 3,
 };
 
-fn printTime(nano: i128) void {
+const CmdArgsState = enum {
+    none,
+    runs,
+    warmup,
+};
+
+var stdout: *std.Io.Writer = undefined;
+
+fn printTime(nano: i128) !void {
     const fnano = @as(f64, @floatFromInt(nano));
 
-    std.debug.print("{d:.2}ms", .{fnano / std.time.ns_per_ms});
+    try stdout.print("{d:.2}ms", .{fnano / std.time.ns_per_ms});
 }
 
 fn testDecode(
@@ -34,7 +42,7 @@ fn testDecode(
 ) !i128 {
     var duration: i128 = 0;
 
-    for (0..options.iters) |_| {
+    for (0..options.runs + options.warmup) |i| {
         //START
         const start_time = std.time.nanoTimestamp();
 
@@ -43,10 +51,13 @@ fn testDecode(
         const end_time = std.time.nanoTimestamp();
         //END
 
-        duration += end_time - start_time;
         image.deinit(allocator);
+
+        if (i < options.warmup) continue;
+
+        duration += end_time - start_time;
     }
-    duration = @divTrunc(duration, options.iters);
+    duration = @divTrunc(duration, options.runs);
 
     return duration;
 }
@@ -61,7 +72,7 @@ fn testEncode(
 
     var duration: i128 = 0;
 
-    for (0..options.iters) |_| {
+    for (0..options.runs + options.warmup) |i| {
         writer.end = 0;
 
         //START
@@ -72,9 +83,11 @@ fn testEncode(
         const end_time = std.time.nanoTimestamp();
         //END
 
+        if (i < options.warmup) continue;
+
         duration += end_time - start_time;
     }
-    duration = @divTrunc(duration, options.iters);
+    duration = @divTrunc(duration, options.runs);
 
     const pixels = try allocator.realloc(buf, writer.buffered().len);
     return .{ .time = duration, .pixels = pixels };
@@ -85,8 +98,6 @@ fn benchFile(
     path: [:0]const u8,
     options: BenchmarkOptions,
 ) !EncDecTime {
-    //std.debug.print("{s}\n", .{path});
-
     if (!std.mem.endsWith(u8, path, ".png")) {
         return .{ .encode = 0, .decode = 0 };
     }
@@ -147,40 +158,159 @@ fn benchmarkDir(
         total_time.encode += res.time.encode;
         total_time.decode += res.time.decode;
         total_images += res.timages;
+
+        if (options.print_individual) {
+            try stdout.print("{s}\n", .{new_path});
+
+            try stdout.print("encode: ", .{});
+            try printTime(res.time.encode);
+            try stdout.print("\n", .{});
+
+            try stdout.print("decode: ", .{});
+            try printTime(res.time.decode);
+            try stdout.print("\n", .{});
+
+            try stdout.flush();
+        }
     }
 
-    std.debug.print("{s}\n", .{path});
+    try stdout.print("{s}\n", .{path});
 
-    std.debug.print("encode: ", .{});
-    printTime(@divTrunc(total_time.encode, total_images));
-    std.debug.print("\n", .{});
+    try stdout.print("encode: ", .{});
+    try printTime(@divTrunc(total_time.encode, total_images));
+    try stdout.print("\n", .{});
 
-    std.debug.print("decode: ", .{});
-    printTime(@divTrunc(total_time.decode, total_images));
-    std.debug.print("\n", .{});
+    try stdout.print("decode: ", .{});
+    try printTime(@divTrunc(total_time.decode, total_images));
+    try stdout.print("\n", .{});
+
+    try stdout.flush();
 
     return .{ .time = total_time, .timages = total_images };
 }
 
+fn printHelp() void {
+    std.debug.print(">>> Help <<<\n", .{});
+    std.debug.print("Usage: zqoi-bench <directory> <options>\n", .{});
+    std.debug.print("    --help             - display this menu\n", .{});
+    std.debug.print("    --runs <number>    - number of runs per image (default 10)\n", .{});
+    std.debug.print("    --warmup <number>  - number of warmup runs (default 3)\n", .{});
+    std.debug.print("    --printindiv       - print times for individual images\n", .{});
+    std.debug.print("Examples:\n", .{});
+    std.debug.print("    zqoi-bench benchmark-dir\n", .{});
+    std.debug.print("    zqoi-bench benchmark-dir/pngimg --runs 5 --warmup 0\n", .{});
+}
+
+
+
 pub fn main() !void {
-    std.debug.print("TODO: ADD CMD ARGS!!!!!!\n^^^^^^^^^^^^^^^^^^^^^^^^\n\n\n", .{});
-
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = if (debug_mode) gpa.allocator()
         else std.heap.smp_allocator;
 
     zstbi.init(allocator);
+    errdefer zstbi.deinit();
     defer zstbi.deinit();
+
+    var stdout_buf: [4096]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
+    stdout = &stdout_writer.interface;
 
     const args = try std.process.argsAlloc(allocator);
     errdefer std.process.argsFree(allocator, args);
     defer std.process.argsFree(allocator, args);
 
-    for (args) |arg| {
-        std.debug.print("{s}\n", .{arg});
+    if (args.len <= 1) {
+        printHelp();
+        return;
     }
 
-    _ = try benchmarkDir(allocator, "../qoi-benchmark-suite", .{});
+    // Check if dir exists
+    const bench_dir_path: [:0]const u8 = args[1];
+
+    var dir = std.fs.cwd().openDir(bench_dir_path, .{}) catch |err| {
+        std.log.err("Couldn't open directory: \"{s}\"", .{bench_dir_path});
+        return err;
+    };
+    dir.close();
+
+    // Args processing
+    var options: BenchmarkOptions = .{};
+    var cmd_state: CmdArgsState = .none;
+
+    for (args[2..]) |arg| {
+        const eql = std.mem.eql;
+
+        if (cmd_state != .none) {
+            switch (cmd_state) {
+                .runs => {
+                    const val = std.fmt.parseInt(
+                        isize,
+                        arg,
+                        10,
+                    ) catch |err| {
+                        std.log.err("Invalid --runs value: \"{s}\"", .{arg});
+                        return err;
+                    };
+
+                    if (val <= 0) {
+                        std.log.err("Value for --runs must be greater than 0\nProvided: \"{s}\"", .{arg});
+                        std.process.exit(1);
+                    }
+
+                    options.runs = @intCast(val);
+                },
+                .warmup => {
+                    const val = std.fmt.parseInt(
+                        isize,
+                        arg,
+                        10,
+                    ) catch |err| {
+                        std.log.err("Invalid --warmup value: \"{s}\"", .{arg});
+                        return err;
+                    };
+
+                    if (val < 0) {
+                        std.log.err("Value for --warmup must be positive\nProvided: \"{s}\"", .{arg});
+                        std.process.exit(1);
+                    }
+
+                    options.warmup = @intCast(val);
+                },
+                else => unreachable,
+            }
+            cmd_state = .none;
+            continue;
+        }
+
+        if (eql(u8, arg, "--help")) {
+            printHelp();
+            return;
+        } else if (eql(u8, arg, "--warmup")) {
+            cmd_state = .warmup;
+        } else if (eql(u8, arg, "--printindiv")) {
+            options.print_individual = true;
+        } else if (eql(u8, arg, "--runs")) {
+            cmd_state = .runs;
+        } else {
+            std.log.err("Invalid argument \"{s}\"", .{arg});
+            std.process.exit(1);
+        }
+    }
+
+    if (cmd_state != .none) {
+        switch (cmd_state) {
+            .runs => {
+                std.log.err("No value for --runs provided", .{});
+            },
+            .warmup => {
+                std.log.err("No value for --warmup provided", .{});
+            },
+            .none => {},
+        }
+        std.process.exit(1);
+    }
+
+    _ = try benchmarkDir(allocator, bench_dir_path, options);
 }
