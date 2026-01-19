b06b4ba92e6ee8ec4eda7675d0985a3e23fc87ee
Pivok <pivoc@protonmail.com>
2026-01-19 17:22:36 +0100

    Style

--- example/build.zig
+++ example/build.zig
@@ -4,15 +4,13 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     const exe = b.addExecutable(.{
         .name = "example",
-        .root_module = exe_mod,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     const zqoi_dep = b.dependency("zqoi", .{
